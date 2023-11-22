352c848814ac7039b89a755b1780b70b4d66dd9d
Rachit Chahar <202151123@iiitvadodara.ac.in>
2023-11-23 01:19:49 +0530

    wallet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,49 @@ import { ThorinGlobalStyles, lightTheme } from '@ensdomains/thorin'
 import Home from './pages/Home'
 import { useState } from 'react';
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  lightTheme as lt
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { polygonMumbai, avalancheFuji } from 'wagmi/chains';
+const { chains, publicClient } = configureChains(
+  [avalancheFuji, polygonMumbai],
+  [publicProvider()]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'CB',
+  projectId: 'cbabb06b3a049fce0e9231318d94998e',
+  chains
+});
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
+
 function App() {
   const [theme, setTheme] = useState(lightTheme)
 
   return (
-   <ThemeProvider theme={theme}>
-      <ThorinGlobalStyles />
-      <Home theme={theme} setTheme={setTheme}/>
-    </ThemeProvider>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} modalSize="compact" coolMode showRecentTransactions={true}
+        theme={lt({
+          accentColor: 'rgb(173,70,213)',
+          accentColorForeground: 'white',
+          borderRadius: 'medium',
+          fontStack: 'system',
+          overlayBlur: 'small',
+        })}>
+        <ThemeProvider theme={theme}>
+          <ThorinGlobalStyles />
+          <Home theme={theme} setTheme={setTheme} />
+        </ThemeProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,38 +1,41 @@
 import styles from './Navbar.module.css'
 import logo from '../images/logo.svg'
-import { useState } from 'react';
-import { Typography, Button, Heading, Profile, lightTheme, darkTheme } from '@ensdomains/thorin';
-import { DotGridSVG, EthSVG, PersonSVG, HouseSVG, MagnifyingGlassActiveSVG, MoonSVG } from '@ensdomains/thorin';
+import { Typography, Heading, Profile, lightTheme, darkTheme } from '@ensdomains/thorin';
+import { DotGridSVG, PersonSVG, HouseSVG, MagnifyingGlassActiveSVG, MoonSVG } from '@ensdomains/thorin';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useAccount } from 'wagmi';
 
-function Navbar({ theme, setTheme }) { 
-  return (
-    <>
-    <div className={styles.header}>
-        <img src={logo} alt="logo" className={styles.logo}/>
-        <Heading color='indigo' className={styles.title}>
-            Guardian-LINK</Heading>
-        <Profile ensName='0x123...456'
-         dropdownItems={[
-             { label: 'Dashboard', onClick: () => null, icon: <DotGridSVG /> },
-             { label: theme===lightTheme?'Dark Mode':'Light Mode', onClick: () => theme===lightTheme?setTheme(darkTheme):setTheme(lightTheme), icon: <MoonSVG />}
-             ]}/>
-    </div>
-    <div className={styles.container}>
-    <nav className={styles.navbar}>
-        <a className={styles.navbarcontent} href="/home">
-            <Typography color='purple' font='large'> <PersonSVG/> Profile</Typography>
-        </a>
-        <a className={styles.navbarcontent} href="/">
-            <Typography color='purple' font='large'><HouseSVG/> Home</Typography>
-        </a>
-        <a className={styles.navbarcontent} href="/">
-            <Typography color='purple' font='large'><MagnifyingGlassActiveSVG/> Search</Typography>
-        </a>
-    </nav>
-        <Button width='44' prefix={<EthSVG/>}> Connect Wallet </Button>
-    </div>
-    </>
-  );
+function Navbar({ theme, setTheme }) {
+    const account = useAccount();
+    return (
+        <>
+            <div className={styles.header}>
+                <img src={logo} alt="logo" className={styles.logo} />
+                <Heading color='indigo' className={styles.title}>
+                    Guardian-LINK</Heading>
+                <Profile ensName={account&&account.address?account.address:'...'}
+                    dropdownItems={[
+                        { label: 'Dashboard', onClick: () => null, icon: <DotGridSVG /> },
+                        { label: theme === lightTheme ? 'Dark Mode' : 'Light Mode', onClick: () => theme === lightTheme ? setTheme(darkTheme) : setTheme(lightTheme), icon: <MoonSVG /> }
+                    ]} />
+            </div>
+            <div className={styles.container}>
+                <nav className={styles.navbar}>
+                    <a className={styles.navbarcontent} href="/home">
+                        <Typography color='purple' font='large'> <PersonSVG /> Profile</Typography>
+                    </a>
+                    <a className={styles.navbarcontent} href="/">
+                        <Typography color='purple' font='large'><HouseSVG /> Home</Typography>
+                    </a>
+                    <a className={styles.navbarcontent} href="/">
+                        <Typography color='purple' font='large'><MagnifyingGlassActiveSVG /> Search</Typography>
+                    </a>
+                </nav>
+                <ConnectButton label='Connect Wallet' accountStatus="address" chainStatus='icon'
+                    showBalance={false} />
+            </div>
+        </>
+    );
 }
 
 export default Navbar;
